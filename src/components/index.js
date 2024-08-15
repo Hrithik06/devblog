a81e3bd22e12b06119f44dda97a03d0676f08eb5
Hrithik06 <el99el03@gmail.com>
2024-08-15 18:57:21 +0530

    signup

--- src/components/index.js
+++ src/components/index.js
@@ -16,7 +16,7 @@ import PostCard from './PostCard';
 import PostCardWrapper from './PostCardWrapper';
 import RTE from './RTE';
 import Select from './Select';
-import Signup from './Signup';
+import Signup from './Signup.jsx';
 import ErrorPage from './ErrorPage';
 export {
     Footer,
